f372209cba9de40edba5f8219a075ba7994fa5c3
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-22 13:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+  if (num <= 0) {
+    return "Input must be a positive integer";
+  }
+
+  // Initialize the first two Fibonacci numbers
+  let fibNums = [0, 1];
+
+  // Calculate Fibonacci numbers up to the num-th position
+  while (fibNums.length <= num) {
+    let nextFib = fibNums[fibNums.length - 1] + fibNums[fibNums.length - 2];
+    fibNums.push(nextFib);
+  }
+
+  // Return the num-th Fibonacci number
+  return fibNums[num - 1];
 }
 
 module.exports = fibonacci;
+
